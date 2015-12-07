52da7ef2e78d4b1da59bfd048288768674c4a9e7
Amanda Dinstel <ajdinstel@gmail.com>
2015-12-06 23:54:40 -0700

    update version test

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,7 +114,8 @@ module.exports = function(grunt) {
       release : {
         options : {
           goal: "release",
-          repositoryId: "ajdinstel",
+          repositoryId: "sphere-sunrise-design-zr",
+          version: "<%= pkg.version %>",
           url: "https://api.bintray.com/maven/ajdinstel/maven/<%= pkg.name %>"
         },
         files: [
